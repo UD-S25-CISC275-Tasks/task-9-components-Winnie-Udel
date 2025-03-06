c29cd62608278ca876d5170441bdc1caadb01692
Winnie-Udel <winnie@udel.edu>
2025-03-06 15:04:44 -0500

    Fixed ShoveBox component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./App.css";
+import { DoubleHalf } from "./bad-components/DoubleHalf";
+import { ChooseTeam } from "./bad-components/ChooseTeam";
+import { ColoredBox } from "./bad-components/ColoredBox";
+import { ShoveBox } from "./bad-components/ShoveBox";
 
 function App(): React.JSX.Element {
     return (
@@ -11,6 +15,22 @@ function App(): React.JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+
+            <hr></hr>
+            <DoubleHalf></DoubleHalf>
+            <hr />
+
+            <hr></hr>
+            <ChooseTeam></ChooseTeam>
+            <hr />
+
+            <hr></hr>
+            <ColoredBox></ColoredBox>
+            <hr />
+
+            <hr></hr>
+            <ShoveBox></ShoveBox>
+            <hr />
         </div>
     );
 }
